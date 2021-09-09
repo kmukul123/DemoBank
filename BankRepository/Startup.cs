80bc72c151fe154d6b3eab795d5baf3702b6b891
POOJAPC\mukul <kmukul@gmail.com>
2021-09-09 19:56:02 +1000

    Adding some startup classes

---
+++ BankRepository/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BankRepository
+{
+    public static class Startup
+    {
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            BankEntity.Startup.ConfigureServices(services);
+            services.AddScoped<Repository.ICustomerRepository, Repository.BankRepository>();
+        }
+    }
+}
